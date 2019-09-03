104f8d411e20cf627df3886c3758913fd4edd668
Dmitrii Shcherbakov <dmitrii.shcherbakov@canonical.com>
2019-09-02 20:21:39 -0400

    Add an option to enable port forwarding feature
    * Adds an option to enable port forwarding service plugin;
    * Exposes whether port forwarding is enabled or not to neutron-gateway
      and neutron-openvswitch charms via the respective relation.
    
    See LP: #1842353
    
    Change-Id: Ic3a8e302942ed331bc3d80223e123c13d61db3b2
    Closes-Bug: #1842353

--- unit_tests/test_neutron_api_hooks.py
+++ unit_tests/test_neutron_api_hooks.py
@@ -90,6 +90,7 @@ TO_PATCH = [
     'generate_ha_relation_data',
     'is_nsg_logging_enabled',
     'is_nfg_logging_enabled',
+    'is_port_forwarding_enabled',
     'remove_old_packages',
     'services',
     'service_restart',
@@ -139,6 +140,7 @@ class NeutronAPIHooksTests(CharmTestCase):
         self.neutron_plugin_attribute.side_effect = _mock_nuage_npa
         self.is_nsg_logging_enabled.return_value = False
         self.is_nfg_logging_enabled.return_value = False
+        self.is_port_forwarding_enabled.return_value = False
 
     def _fake_relids(self, rel_name):
         return [randrange(100) for _count in range(2)]
@@ -529,6 +531,7 @@ class NeutronAPIHooksTests(CharmTestCase):
             'neutron-api-ready': 'no',
             'enable-nsg-logging': False,
             'enable-nfg-logging': False,
+            'enable-port-forwarding': False,
             'global-physnet-mtu': 1500,
             'physical-network-mtus': None,
         }
@@ -574,6 +577,7 @@ class NeutronAPIHooksTests(CharmTestCase):
             'neutron-api-ready': 'no',
             'enable-nsg-logging': True,
             'enable-nfg-logging': False,
+            'enable-port-forwarding': False,
             'global-physnet-mtu': 1500,
             'physical-network-mtus': None,
         }
@@ -625,6 +629,7 @@ class NeutronAPIHooksTests(CharmTestCase):
             'neutron-api-ready': 'no',
             'enable-nsg-logging': False,
             'enable-nfg-logging': True,
+            'enable-port-forwarding': False,
             'global-physnet-mtu': 1500,
             'physical-network-mtus': None,
         }
@@ -639,6 +644,57 @@ class NeutronAPIHooksTests(CharmTestCase):
 
         self.test_config.set('enable-firewall-group-logging', True)
         self.is_nfg_logging_enabled.return_value = True
+
+        self._call_hook('neutron-plugin-api-relation-joined')
+
+        self.relation_set.assert_called_with(
+            relation_id=None,
+            **_relation_data)
+
+    def test_neutron_plugin_api_relation_joined_port_forwarding(self):
+        self.unit_get.return_value = '172.18.18.18'
+        self.IdentityServiceContext.return_value = \
+            DummyContext(return_value={})
+        _relation_data = {
+            'neutron-security-groups': False,
+            'enable-dvr': False,
+            'enable-l3ha': False,
+            'enable-qos': False,
+            'enable-vlan-trunking': False,
+            'addr': '172.18.18.18',
+            'polling-interval': 2,
+            'rpc-response-timeout': 60,
+            'report-interval': 30,
+            'l2-population': False,
+            'overlay-network-type': 'vxlan',
+            'service_protocol': None,
+            'auth_protocol': None,
+            'service_tenant': None,
+            'service_port': None,
+            'region': 'RegionOne',
+            'service_password': None,
+            'auth_port': None,
+            'auth_host': None,
+            'service_username': None,
+            'service_host': None,
+            'neutron-api-ready': 'no',
+            'enable-nsg-logging': False,
+            'enable-nfg-logging': False,
+            'enable-port-forwarding': True,
+            'global-physnet-mtu': 1500,
+            'physical-network-mtus': None,
+        }
+
+        self.is_qos_requested_and_valid.return_value = False
+        self.is_vlan_trunking_requested_and_valid.return_value = False
+        self.get_dvr.return_value = False
+        self.get_l3ha.return_value = False
+        self.get_l2population.return_value = False
+        self.get_overlay_network_type.return_value = 'vxlan'
+        self.get_dns_domain.return_value = ''
+
+        self.test_config.set('enable-port-forwarding', True)
+        self.is_port_forwarding_enabled.return_value = True
 
         self._call_hook('neutron-plugin-api-relation-joined')
 
@@ -675,6 +731,7 @@ class NeutronAPIHooksTests(CharmTestCase):
             'neutron-api-ready': 'no',
             'enable-nsg-logging': False,
             'enable-nfg-logging': False,
+            'enable-port-forwarding': False,
             'global-physnet-mtu': 1500,
             'physical-network-mtus': None,
         }
@@ -720,6 +777,7 @@ class NeutronAPIHooksTests(CharmTestCase):
             'neutron-api-ready': 'no',
             'enable-nsg-logging': False,
             'enable-nfg-logging': False,
+            'enable-port-forwarding': False,
             'global-physnet-mtu': 1500,
             'physical-network-mtus': None,
         }
@@ -767,6 +825,7 @@ class NeutronAPIHooksTests(CharmTestCase):
             'neutron-api-ready': 'no',
             'enable-nsg-logging': False,
             'enable-nfg-logging': False,
+            'enable-port-forwarding': False,
             'global-physnet-mtu': 1500,
             'physical-network-mtus': None,
         }
@@ -813,6 +872,7 @@ class NeutronAPIHooksTests(CharmTestCase):
             'dns-domain': 'openstack.example.',
             'enable-nsg-logging': False,
             'enable-nfg-logging': False,
+            'enable-port-forwarding': False,
             'global-physnet-mtu': 1500,
             'physical-network-mtus': None,
         }
